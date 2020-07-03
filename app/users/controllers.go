7d33f24b2d0907e4a22361d507ea9239125ebb86
Farhan <farhan1ahmed@hotmail.com>
2020-07-03 16:42:07 +0500

    logout handler updated with middleware

--- app/users/controllers.go
+++ app/users/controllers.go
@@ -1,10 +1,13 @@
 package users
 
-import "net/http"
+import (
+	"github.com/farhan1ahmed/GoLang_ToDoApp/app/auth"
+	"net/http"
+)
 
 func handleRequests(uApp *UserApp) {
 	http.HandleFunc("/register", uApp.registerUser)
 	http.HandleFunc("/confirm/", uApp.confirmUser)
 	http.HandleFunc("/login", uApp.loginUser)
-	http.HandleFunc("/logout", uApp.logoutUser)
+	http.Handle("/logout", auth.AuthMiddleware(http.HandlerFunc(uApp.logoutUser)))
 }
